bee588ad69269fa8567a9a081d4f1fac09aa8551
Schneider Roland <rocho02@gmail.com>
2013-08-30 19:47:10 +0200

    inital yii commit

---
+++ protected/migrations/m130519_155432_create_user_table.php
@@ -0,0 +1,36 @@
+<?php
+
+class m130519_155432_create_user_table extends CDbMigration
+{
+	public function up()
+	{
+		//create the user table
+		$this->createTable('tbl_user', array(
+				'id' => 'pk',
+				'username' => 'string NOT NULL',
+				'email' => 'string NOT NULL',
+				'password' => 'string NOT NULL',
+				'last_login_time' => 'datetime DEFAULT NULL',
+				'create_time' => 'datetime DEFAULT NULL',
+				'create_user_id' => 'int(11) DEFAULT NULL',
+				'update_time' => 'datetime DEFAULT NULL',
+				'update_user_id' => 'int(11) DEFAULT NULL',
+		), 'ENGINE=InnoDB');
+	}
+
+	public function down()
+	{
+		$this->dropTable('tbl_user');
+	}
+
+	/*
+	// Use safeUp/safeDown to do migration with transaction
+	public function safeUp()
+	{
+	}
+
+	public function safeDown()
+	{
+	}
+	*/
+}
---
+++ protected/runtime/gii-1.1.13/ModelCode.php
@@ -0,0 +1,9 @@
+<?php
+return array (
+  'template' => 'default',
+  'connectionId' => 'db',
+  'tablePrefix' => 'tbl_',
+  'modelPath' => 'application.modules.auth.models',
+  'baseClass' => 'CActiveRecord',
+  'buildRelations' => '0',
+);
