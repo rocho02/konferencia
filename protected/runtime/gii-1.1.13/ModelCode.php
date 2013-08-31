8db98e63e1d1bfabddb2db8ddd85df48e15ec770
Schneider Roland <rocho02@gmail.com>
2013-08-31 19:00:55 +0200

    gitignore changes

--- protected/runtime/gii-1.1.13/ModelCode.php
+++
@@ -1,9 +0,0 @@
-<?php
-return array (
-  'template' => 'default',
-  'connectionId' => 'db',
-  'tablePrefix' => 'tbl_',
-  'modelPath' => 'application.modules.auth.models',
-  'baseClass' => 'CActiveRecord',
-  'buildRelations' => '0',
-);
