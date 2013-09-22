08e2236aab6b3a9791a08eb2fe7829f798101f70
Schneider Roland <rocho02@gmail.com>
2013-09-22 17:53:27 +0200

    migration changes

--- protected/migrations/m130922_151830_role_management_changes.php
+++ protected/migrations/m130922_151830_role_management_changes.php
@@ -7,30 +7,48 @@ class m130922_151830_role_management_changes extends CDbMigration
 	{
 		$this->_authManager=Yii::app()->authManager;
 		
-				$this->insert('tbl_user', array(
-				'ID'=> '3',
-				'username' => 'organizer',
-				'email' => 'organizer@yii.com',
-				'password' => '098f6bcd4621d373cade4e832627b4f6',
-				'last_login_time' => 'NULL',
-				'create_time' => '2013-08-30 12:00',
-				'create_user_id' => '1',
-				'update_time' => 'NULL',
-				'update_user_id' => 'NULL',
-		)
-	); 
-	$this->_authManager->createOperation("Event.Create", "create event");
-	$role=$this->_authManager->getAuthItem("admin");
-	
-			 $role->addChild("Event.Create"); 
-			 $role->addChild("Event.Update"); 
-			 $role->addChild("Event.Delete"); 
-			 $role->addChild("Event.Read"); 
-	}
+					$this->insert('tbl_user', array(
+							'ID'=> '3',
+							'username' => 'organizer',
+							'email' => 'organizer@yii.com',
+							'password' => '098f6bcd4621d373cade4e832627b4f6',
+							'last_login_time' => 'NULL',
+							'create_time' => '2013-08-30 12:00',
+							'create_user_id' => '1',
+							'update_time' => 'NULL',
+							'update_user_id' => 'NULL' ,
+						)
+					);
+					
+					$this->_authManager->createOperation("Event.Create",	"create event");
+					$this->_authManager->createOperation("Event.Update",	"update event");
+					$this->_authManager->createOperation("Event.Delete",	"Delete event");
+					$this->_authManager->createOperation("Event.Index",		"List events");
+ 
+					//létező admin role betöltése
+					$role=$this->_authManager->getAuthItem("admin");
+					 
+					//új operationök hozzáadása
+					$role->addChild("Event.Create");
+					$role->addChild("Event.Update");
+					$role->addChild("Event.Delete"); 
+					$role->addChild("Event.Index");
 
-	public function down()
-	{
-		echo "m130922_151830_role_management_changes does not support migration down.\n";
+					//új role létrehozása
+					$role=$this->_authManager->createRole("organizer","organizer");
+					
+					//organizer role-hoz operationok hozzáadása
+					$role->addChild("Event.Create");
+					$role->addChild("Event.Update");
+					$role->addChild("Event.Delete");
+					$role->addChild("Event.Index");
+					
+					}
+
+					public function down()
+					{
+					echo
+ "m130922_151830_role_management_changes does not support migration down.\n";
 		return false;
 	}
 
