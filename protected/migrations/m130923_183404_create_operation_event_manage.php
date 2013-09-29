e6c3eb42e0a9bdb918ad148077ca069d84849ed0
Gabor <horvy86@gmail.com>
2013-09-29 14:01:25 +0200

    create_operation_event migration

---
+++ protected/migrations/m130923_183404_create_operation_event_manage.php
@@ -0,0 +1,25 @@
+<?php
+
+class m130923_183404_create_operation_event_manage extends CDbMigration
+{
+	public function up()
+	{
+	}
+
+	public function down()
+	{
+		echo "m130923_183404_create_operation_event_manage does not support migration down.\n";
+		return false;
+	}
+
+	/*
+	// Use safeUp/safeDown to do migration with transaction
+	public function safeUp()
+	{
+	}
+
+	public function safeDown()
+	{
+	}
+	*/
+}
---
+++ protected/migrations/m130923_191249_event_manage_operation.php
@@ -0,0 +1,25 @@
+<?php
+
+class m130923_191249_event_manage_operation extends CDbMigration
+{
+	public function up()
+	{
+	}
+
+	public function down()
+	{
+		echo "m130923_191249_event_manage_operation does not support migration down.\n";
+		return false;
+	}
+
+	/*
+	// Use safeUp/safeDown to do migration with transaction
+	public function safeUp()
+	{
+	}
+
+	public function safeDown()
+	{
+	}
+	*/
+}
