4d64ad5c7967a2d2099fd5e520717bb3efd3de26
Schneider Roland <rocho02@gmail.com>
2013-10-11 23:15:42 +0200

    message changes

--- protected/views/layouts/main.php
+++ protected/views/layouts/main.php
@@ -33,7 +33,7 @@
 				array('label'=>'About', 'url'=>array('/site/page', 'view'=>'about')),
 				array('label'=>'Contact', 'url'=>array('/site/contact')),
 				array('label'=>'Users', 'url'=>array('/auth/user')),
-				array('label'=>'Üzenetek', 'url'=>array('/message/index')),
+				array('label'=>'Üzenetek', 'url'=>array('/message/incoming')),
 				array('label'=>'Konferencia', 'url'=>array('/event/index')),
 				array('label'=>'Téma', 'url'=>array('/topic/index')),
 				array('label'=>'Cikkeim', 'url'=>array('/article/index'),'visible'=>!Yii::app()->user->isGuest),
