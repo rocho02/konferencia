58493ac03aad5327d991e191f97e7ff2ae060860
Gabor <horvy86@gmail.com>
2013-10-16 22:53:44 +0200

    main.php changed

--- protected/config/main.php
+++ protected/config/main.php
@@ -7,7 +7,7 @@
 // CWebApplication properties can be configured here.
 return array(
 	'basePath'=>dirname(__FILE__).DIRECTORY_SEPARATOR.'..',
-	'name'=>'Konferencia menedzment rendszer',
+	'name'=>'Konferencia menedzsment rendszer',
 	'language'=>'hu',
 	// preloading 'log' component
 	'preload'=>array('log'),
