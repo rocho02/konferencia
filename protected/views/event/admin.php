9594bffe46bf7adfefc4b2dbe1b8da7a4282c026
Gabor <horvy86@gmail.com>
2013-11-04 21:41:01 +0100

    event view changed

--- protected/views/event/admin.php
+++ protected/views/event/admin.php
@@ -26,7 +26,7 @@ $('.search-form form').submit(function(){
 ");
 ?>
 
-<h1>Konferenciák szerkesztése</h1>
+<h1>Konferenciák menedzselése</h1>
 
 <p>
 You may optionally enter a comparison operator (<b>&lt;</b>, <b>&lt;=</b>, <b>&gt;</b>, <b>&gt;=</b>, <b>&lt;&gt;</b>
